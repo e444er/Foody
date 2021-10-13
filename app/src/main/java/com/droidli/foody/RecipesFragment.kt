1a157c1925bc20020a478fe8d0620fe39bca3897
e444er <e444er@@gmail.com>
2021-10-13 15:39:27 +0600

    ui recipe shimmer

--- app/src/main/java/com/droidli/foody/RecipesFragment.kt
+++ app/src/main/java/com/droidli/foody/RecipesFragment.kt
@@ -3,10 +3,15 @@ package com.droidli.foody
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.droidli.foody.databinding.RecipesFragmentBinding
 
-class RecipesFragment: Fragment(R.layout.recipes_fragment) {
+class RecipesFragment : Fragment(R.layout.recipes_fragment) {
+
+    private val binding by viewBinding(RecipesFragmentBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.shimmerRecyclerView.showShimmer()
     }
 }
